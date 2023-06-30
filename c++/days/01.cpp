1a25b2f71d3a76a147e91e04000be96fc6f242ce
Oscar Ablinger <dev.ablinger@gmail.com>
2023-06-30 23:13:57 +0200

    feat(c01): implement day 1 in c++

--- c++/days/01.cpp
+++ c++/days/01.cpp
@@ -1,19 +1,74 @@
-#include "01.h"
+#include <algorithm>
+#include <numeric>
 
-#include <iostream>
+#include "01.h"
 
 namespace aoc {
   namespace day01 {
 	using std::string;
 	using std::vector;
 
+
+	string part_a(vector<string> input) {
+	  int currentCalories = 0;
+	  int maxCalories = 0;
+
+	  for (auto line : input) {
+		if (line == "") {
+		  if (currentCalories > maxCalories) {
+			maxCalories = currentCalories;
+		  }
+		  currentCalories = 0;
+		}
+		else
+		{
+		  currentCalories += stoi(line);
+		}
+	  }
+	  if (currentCalories > maxCalories) {
+		maxCalories = currentCalories;
+	  }
+	  
+	  return std::to_string(maxCalories);
+	}
+
+	string part_b(vector<string> input) {
+	  int currentCalories = 0;
+	  vector<int> caloriesSums;
+	  for (auto line : input) {
+		if (line == "") {
+		  caloriesSums.push_back(currentCalories);
+		  currentCalories = 0;
+		}
+		else
+		{
+		  currentCalories += stoi(line);
+		}
+	  }
+	  caloriesSums.push_back(currentCalories);
+
+	  vector<int> top3(3);
+	  std::partial_sort_copy(
+		caloriesSums.begin(), caloriesSums.end(),
+		top3.begin(), top3.end(),
+		std::greater<int>()
+	  );
+	  return std::to_string(std::accumulate(top3.begin(), top3.end(), 0));
+	}
+
+
 	string run(part_t part, vector<string> input) {
 
-	  std::cout << "input:" << std::endl;
-	  for (auto line : input)
-		std::cout << line << std::endl;
-
-	  return input[0];
+	  switch (part) {
+	  case partA:
+		return part_a(input);
+		break;
+	  case partB:
+		return part_b(input);
+		break;
+	  default:
+		exit(1);
+	  }
 	}
   }
 }
